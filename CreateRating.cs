cf53fb473022b9a441b79596a61e00a042e9c595
Iheb Khemissi <iheb.khemissi@gmail.com>
2022-11-30 14:12:18 +0100

    feat: Store ratings in CosmosDB

--- CreateRating.cs
+++ CreateRating.cs
@@ -12,16 +12,21 @@ namespace Company.Function
 {
     public class CreateRating
     {
+        private readonly HttpClient _httpClient;
         private readonly ILogger _logger;
 
-        public CreateRating(ILoggerFactory loggerFactory)
+ 
+
+        public CreateRating(HttpClient httpClient, ILoggerFactory loggerFactory)
         {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(Environment.GetEnvironmentVariable("ApiURI"));
             _logger = loggerFactory.CreateLogger<CreateRating>();
         }
 
         [Function("CreateRating")]
         [CosmosDBOutput("%CosmosDb%", "%CosmosCollOut%", ConnectionStringSetting = "CosmosConnection", CreateIfNotExists = true)]
-        public async static Task<object> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
+        public async Task<object> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
         {
             // _logger.LogInformation("C# HTTP trigger function processed a request.");
 
@@ -32,7 +37,17 @@ namespace Company.Function
             RatingItem ratingItem = (RatingItem)Newtonsoft.Json.JsonConvert.DeserializeObject(requestBody, typeof(RatingItem));
 
             // validate userId and productId using external APIs
+            HttpResponseMessage responseProduct = await _httpClient.GetAsync($"GetProduct?productId={ratingItem.productId}");
+            HttpResponseMessage responseUser = await _httpClient.GetAsync($"GetUser?userId={ratingItem.userId}");
 
+            if (!responseProduct.IsSuccessStatusCode || !responseUser.IsSuccessStatusCode)
+            {
+
+                var invalidUserAndProductResponse = req.CreateResponse(HttpStatusCode.BadRequest);
+                invalidUserAndProductResponse.WriteString("Invalid user or product reference");
+                return null;
+
+            }
 
             // validate the value of rating and ensure it is between 0 and 5
             if (ratingItem.rating < 0 || ratingItem.rating > 5) {
